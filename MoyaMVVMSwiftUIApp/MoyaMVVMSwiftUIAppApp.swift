ff310d7f6139c9cdc8fdd8a04aafd4d72479915b
蔡万鸿 <wanhongcai@sohu-inc.com>
2025-08-01 14:43:17 +0800

    Initial Commit

---
+++ MoyaMVVMSwiftUIApp/MoyaMVVMSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoyaMVVMSwiftUIAppApp.swift
+//  MoyaMVVMSwiftUIApp
+//
+//  Created by caiwanhong on 2025/8/1.
+//
+
+import SwiftUI
+
+@main
+struct MoyaMVVMSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
